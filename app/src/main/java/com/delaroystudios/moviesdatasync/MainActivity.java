c9e43a34b951515074a767434f7e0c5db69be841
Delaroy Studios <bamoyk@yahoo.com>
2017-08-21 12:04:00 +0100

    MoviesProvider

--- app/src/main/java/com/delaroystudios/moviesdatasync/MainActivity.java
+++ app/src/main/java/com/delaroystudios/moviesdatasync/MainActivity.java
@@ -3,11 +3,20 @@ package com.delaroystudios.moviesdatasync;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.delaroystudios.moviesdatasync.data.MovieDbHelper;
+import com.delaroystudios.moviesdatasync.sync.MovieSyncAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    MovieDbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dbHelper = new MovieDbHelper(this);
+        MovieSyncAdapter.initializeSyncAdapter(this);
+
     }
 }
